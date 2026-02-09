515451f1ba993cbc7c1900cb32ed7f874ed99400
Fariha Mansoor <you@example.com>
2026-02-09 22:38:27 +0500

    modified

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ if(season === 'summer'){
     <div class="summer season d-flex justify-content-center align-items-center">
     <div class="card season-card">
       <img src="./summer-card.gif" class="card-img-top img-fluid" alt="summer card">
-      <div class="card-body">
-        <h5 class="card-title fs-4 mb-2 text-white">Summer Season Card ☀️</h5>
+      <div class="card-body p-3">
+        <h5 class="card-title fw-bold fs-4 mb-2 text-white">Summer Season Card ☀️</h5>
         <p class="card-text">Summer is a hot season with bright sunshine and warm days.</p>
   
       </div>
@@ -17,8 +17,8 @@ else if(season === 'winter'){
      <div class="winter season d-flex justify-content-center align-items-center">
     <div class="card season-card">
       <img src="./winter-season-card.gif" class="card-img-top img-fluid" alt="summer card">
-      <div class="card-body">
-        <h5 class="card-title fs-4 mb-2 text-white">Winter Season Card ❄️</h5>
+      <div class="card-body p-3">
+        <h5 class="card-title fw-bold fs-4 mb-2 text-white">Winter Season Card ❄️</h5>
         <p class="card-text">Winter is a cold season with chilly weather and cool winds.</p>
   
       </div>
@@ -30,8 +30,8 @@ else if(season === 'rainy'){
    <div class="rainy season d-flex justify-content-center align-items-center">
     <div class="card season-card">
       <img src="./rainy-card.gif" class="card-img-top img-fluid" alt="summer card">
-      <div class="card-body">
-        <h5 class="card-title fs-4 mb-2 text-white">Rainy Season Card ⛈️</h5>
+      <div class="card-body p-2">
+        <h5 class="card-title fw-bold fs-4 mb-2 text-white">Rainy Season Card ⛈️</h5>
         <p class="card-text">Rainy season brings frequent rainfall, cool breeze, and cloudy skies. It refreshes nature and fills rivers and fields with water.</p>
   
       </div>
@@ -43,8 +43,8 @@ else if(season === 'spring'){
     <div class="spring season d-flex justify-content-center align-items-center">
     <div class="card season-card">
       <img src="./spring-card.gif" class="card-img-top img-fluid" alt="summer card">
-      <div class="card-body">
-        <h5 class="card-title fs-4 mb-2 text-white">Spring Season Card 🌸</h5>
+      <div class="card-body p-3">
+        <h5 class="card-title fw-bold fs-4 mb-2 text-white">Spring Season Card 🌸</h5>
         <p class="card-text">Spring is a pleasant season when flowers bloom and the weather becomes mild.</p>
   
       </div>
@@ -57,8 +57,8 @@ else if(season === 'autumn'){
     <div class="autumn season d-flex justify-content-center align-items-center">
     <div class="card season-card">
       <img src="./autumn-card.gif" class="card-img-top img-fluid" alt="summer card">
-      <div class="card-body">
-        <h5 class="card-title fs-4 mb-2 text-white">Autumn Season Card 🍁๋࣭</h5>
+      <div class="card-body p-3">
+        <h5 class="card-title fw-bold fs-4 mb-2 text-white">Autumn Season Card 🍁๋࣭</h5>
         <p class="card-text">Autumn is marked by falling leaves, cooler temperatures, and a calm atmosphere.</p>
   
       </div>
